acadddf3388ee4d166e74285b5bffaffa039fe6a
Ryan Cornett <ryandcornett@gmail.com>
2023-09-12 16:30:42 -0400

    Error handling for initial prompt

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿using sdc1_knowledge_check_2;
 
-Console.WriteLine("How many records do you want to add? ");
-var numberOfRecords = int.Parse(Console.ReadLine());
+bool properInput = false;
+int result = 0;
+while (properInput == false)
+{
+    Console.WriteLine("How many records do you want to add? ");
+    string numberOfRecords = Console.ReadLine();
+    if (Int32.TryParse(numberOfRecords, out result)) { properInput = true; }
+    else { Console.WriteLine("Invalid input."); }
+}
 
 var recordList = new List<AleEightOne>();
 AppLogic appLogic = new AppLogic();
-for (int i = 0; i < numberOfRecords; i++)
+for (int i = 0; i < result; i++)
 {
     var myClass = new AleEightOne();
     string userInput = "";
